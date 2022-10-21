23cb3617049fd80d2dc4a1ef0de333087fc7c770
john.tc.tsai\DEV <shinn716@livemail.tw>
2022-10-21 10:58:08 +0800

    Add osc.

---
+++ Assets/HopeFun/scripts/osc/OSCReceiver.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OSCReceiver : MonoBehaviour
+{
+    public static OSCReceiver Instance;
+
+    [SerializeField] private OSC oscReference;
+
+    private string msg = string.Empty;
+    private SceneController sceneController;
+
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    private void Start()
+    {
+        sceneController = GetComponent<SceneController>();
+        oscReference.SetAllMessageHandler(OnReceive);
+    }
+
+    private void OnReceive(OscMessage _message)
+    {
+        print("OnReceive: " + _message.address + " Value: " + _message.values[0].ToString());
+        switch (_message.address)
+        {
+            case "/scene":
+                msg = _message.values[0].ToString();
+                if (msg.Equals("ready"))
+                    sceneController.LoadScene(0);
+                else if (msg.Equals("scene1"))
+                    sceneController.LoadScene(1);
+                else if (msg.Equals("scene2"))
+                    sceneController.LoadScene(2);
+                break;
+            case "/app":
+                msg = _message.values[0].ToString();
+                if (msg.Equals("close"))
+                    Application.Quit();
+                break;
+            case "/countdiwn":
+                msg = _message.values[0].ToString();
+                int.TryParse(msg, out int _tmp);
+                SceneController.Minutes = _tmp;
+                break;
+        }
+    }
+}
